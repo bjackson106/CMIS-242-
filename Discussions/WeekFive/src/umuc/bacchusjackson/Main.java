39dea262ee367368f023d257d94db2589909ab1a
bacchus.jackson <bjjackson32@gmail.com>
2019-11-05 21:17:03 -0600

    Added week 5 recursion example

---
+++ Discussions/WeekFive/src/umuc/bacchusjackson/Main.java
@@ -0,0 +1,34 @@
+package umuc.bacchusjackson;
+
+public class Main {
+    public static void main(String[] args) {
+        int numberOfNodes = 11;
+
+        int recursiveTotal = countNodeLinksRecursive(numberOfNodes);
+
+        System.out.println("Number of Node Links (Recursive): " + recursiveTotal);
+
+        int iterativeTotal = countNodeLinksIterative(numberOfNodes);
+
+        System.out.println("Number of Node Links (Iterative): " + iterativeTotal);
+    }
+
+    static int countNodeLinksRecursive(int nodes) {
+        if (nodes <=1) {
+            return 0;
+        }
+        if (nodes == 2) {
+            return 1;
+        }
+
+        return countNodeLinksRecursive(nodes - 1) + (nodes - 1);
+    }
+
+    static int countNodeLinksIterative(int nodes) {
+        int total = 0;
+        for(int i = 1; i < nodes; i++) {
+            total = total + (nodes - i);
+        }
+        return total;
+    }
+}
